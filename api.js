cfae12f2d036528d7257ae5813c30c7c75e09568
Prospercashhub <ekeprospee9@gmail.com>
2026-08-01 16:08:08 +0100

    Update API_BASE URL to production endpoint

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // Cash Hub NG V9 API adapter
 // Set API_BASE to your deployed API URL, then migrate frontend pages from localStorage to these functions.
-const API_BASE = "http://localhost:3000";
+const API_BASE = "https://cash-hub-ng.onrender.com";
 async function api(path, options={}) {
   const r = await fetch(API_BASE + path, {credentials:"include", headers:{"Content-Type":"application/json",...(options.headers||{})}, ...options});
   const data = await r.json().catch(()=>({}));
