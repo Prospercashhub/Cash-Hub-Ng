6e13ae8068ee001553fc767ef14b051994a71104
Prospercashhub <ekeprospee9@gmail.com>
2026-08-13 19:28:23 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,34 +1,22 @@
 // Cash Hub NG API adapter
-// Central connection to the deployed backend.
 
 const API_BASE = "https://cash-hub-ng2.onrender.com";
 
 async function api(path, options = {}) {
-  const fetchOptions = {
-    ...options,
-    credentials: "omit",
+  const response = await fetch(API_BASE + path, {
+    credentials: "include",
     headers: {
       "Content-Type": "application/json",
       ...(options.headers || {})
-    }
-  };
+    },
+    ...options
+  });
 
-  const response = await fetch(
-    API_BASE + path,
-    fetchOptions
-  );
-
-  const data = await response
-    .json()
-    .catch(() => ({}));
+  const data = await response.json().catch(() => ({}));
 
   if (!response.ok) {
-    throw new Error(
-      data.error || "Request failed"
-    );
+    throw new Error(data.error || "Request failed");
   }
 
   return data;
-}
-
-window.api = api;
+          }
